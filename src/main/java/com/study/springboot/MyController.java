a08100e8268357922d6cd7ee86a480aea8b93f18
ssujiny <aldebaran97@naver.com>
2024-12-16 00:00:46 +0900

    fourth Valid annotation practice

--- src/main/java/com/study/springboot/MyController.java
+++ src/main/java/com/study/springboot/MyController.java
@@ -15,7 +15,7 @@ public class MyController {
 	
 	@GetMapping("/")
 	public @ResponseBody String root() throws Exception {
-		return "Valid_initBinder (3)";
+		return "Valid_Annotation (4)";
 	}
 	
 	@GetMapping("/insertForm")
@@ -38,10 +38,14 @@ public class MyController {
 			//System.out.println("getAllErrors : " + result.getAllErrors());
 			
 			if(result.getFieldError("writer") != null) {
-				System.out.println("1: " + result.getFieldError("writer").getCode());
+				// ~ 3.
+				//System.out.println("1: " + result.getFieldError("writer").getCode());
+				System.out.println("1: " + result.getFieldError("writer").getDefaultMessage());
 			}
 			if(result.getFieldError("content") != null) {
-				System.out.println("2: " + result.getFieldError("content").getCode());
+				// ~ 3.
+				//System.out.println("2: " + result.getFieldError("content").getCode());
+				System.out.println("2: " + result.getFieldError("content").getDefaultMessage());
 			}
 			
 			page = "createPage";
@@ -52,13 +56,13 @@ public class MyController {
 	 	1) 파라미터로 객체 변수 들어오면
 	 	2) 스프링이 binder 변수에 저장된 객체 통해서 즉시 유효성 검사
 	 	3) 에러가 있다면 result 변수에 저장
-	 */
 	
+	3. initBinder 버전
 	// 프로젝트 시작시 먼저 실행되어 메서드 등록
 	@InitBinder
 	protected void initBinder(WebDataBinder binder) {
 		//약한 결합 상태 
 		binder.setValidator(new ContentValidator());
 	}
-	
+	 */
 }
